dfee95373463e05ccc6e28942ad5cd93661c7584
Leandro Barbosa <leandrobar93@gmail.com>
2016-11-19 19:31:47 -0200

    add data into html home

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,11 +1,21 @@
 # coding: utf-8
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.urls import reverse
 
 
 class HomeViewTest(TestCase):
-    def test_correct_response(self):
+    @patch('core.views.HomeView.get_all_comments')
+    def test_correct_response(self, _comments):
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/index.html')
+
+    @patch('core.views.HomeView.get_all_comments')
+    def test_comments_context_in_html(self, _comments):
+        response = self.client.get(reverse('home'))
+
+        self.assertIn('comments', response.context)
+        _comments.assert_called_once_with()
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
 # coding: utf-8
 from django.views.generic import TemplateView
 
+import requests
+
 
 class HomeView(TemplateView):
     template_name = 'core/index.html'
+
+    def get_all_comments(self):
+        response = requests.get(
+            'https://facebook-downloader.herokuapp.com/download/?query=nytimes'
+            '/posts?fields=comments.limit(1),permalink_url&limit=1')
+
+        return response.json()
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['comments'] = self.get_all_comments()
+
+        return context
