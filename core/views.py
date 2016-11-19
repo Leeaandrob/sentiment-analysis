4d3282b9a6640931dba5a9c83b3713f846d74808
Leandro Barbosa <leandrobar93@gmail.com>
2016-11-19 20:27:14 -0200

    diplay some information post page on html

--- core/views.py
+++ core/views.py
@@ -10,12 +10,12 @@ class HomeView(TemplateView):
     def get_all_comments(self):
         response = requests.get(
             'https://facebook-downloader.herokuapp.com/download/?query=nytimes'
-            '/posts?fields=comments.limit(50),permalink_url&limit=2')
+            '/posts?fields=comments.limit(50),full_picture&limit=2')
 
         return response.json()
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['comments'] = self.get_all_comments()
+        context['comments'] = self.get_all_comments().get('data')
 
         return context
